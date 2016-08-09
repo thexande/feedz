3c43753a2148de7ca7b53e802924216ee471c55d
Alexander Murphy <amurphy345@gmail.com>
2016-08-09 17:13:46 -0600

    dynamic loop rooute

--- app/app.js
+++ app/app.js
@@ -1,19 +1,24 @@
-var express = require('express')
-var path = require('path')
-var favicon = require('serve-favicon')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
-var path = require('path')
-var spa = require('./routes/spa')
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const spa = require('./routes/spa')
+const jsonAPI = require('./routes/routeExport')
+const userRoute = require('./routes/jsonApi/user')
 require('dotenv').config()
-var app = express()
+const app = express()
 app.set('views', path.join(__dirname, 'views'));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/public')))
+for(let route in jsonAPI){
+  if(jsonAPI.hasOwnProperty(route)){
+    let routeString = '/' +   route.toString()
+    app.use('/users', jsonAPI[route])
+  }
+}
 app.use('/', spa)
 app.use('/nm', express.static(__dirname + '/../node_modules/'))
 app.use((req, res, next) => {
@@ -23,9 +28,6 @@ app.use((req, res, next) => {
 })
 app.use((err, req, res, next) => {
   res.status(err.status || 500)
-  res.json({
-    message: err.message,
-    error: {}
-  })
+  res.json({ message: err.message })
 })
 module.exports = app
--- app/routes/jsonApi/user.js
+++ app/routes/jsonApi/user.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path')
-const userModelClass = require('../../../Models/authModels/userModel')
+const userModelClass = require('../../Models/authModels/userModel')
 const userModel = new userModelClass
 
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../../public/build/index.html'))
-})
-router.route('/test')
+
+router.route('/')
     .get((req, res, next) => {
         userModel.getUser().then((resp) => {res.json(resp)})
     })

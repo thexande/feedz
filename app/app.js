8ccae573bcc6b982a70d3bee9153382697555730
Alexander Murphy <amurphy345@gmail.com>
2016-08-11 10:51:28 -0600

    adding coors headers

--- app/app.js
+++ app/app.js
@@ -11,7 +11,21 @@ const passport = require('passport')
 const session = require('express-session')
 require('dotenv').config()
 const app = express()
- app.use(passport.initialize());
+// CORS headers
+app.use(function (req, res, next) {
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+app.use(passport.initialize());
 app.use(passport.session());
 app.set('views', path.join(__dirname, 'views'));
 app.use(logger('dev'))

de4adfeb18f14d0947930bc9ff0fca2c85ea3ef7
Alexander Murphy <amurphy345@gmail.com>
2016-08-12 13:36:48 -0600

    TRUMP STEAKS. 3 column post view looking great

--- app/public/js/angular/controllers/postController.js
+++ app/public/js/angular/controllers/postController.js
@@ -11,7 +11,14 @@
     while($scope.feed.length) {
       $scope.feedColumns.push($scope.feed.splice(0, $scope.feedColumnLength))
     }
-
+    if($scope.feedColumns.length === 4){
+      $scope.feedColumns[3].forEach((val, key) => {
+        console.log(val)
+        $scope.feedColumns[ Math.floor(Math.random() * (2 - 0 + 1)) + 0].push(val)
+      })
+      $scope.feedColumns.pop()
+    }
+    console.log($scope.feedColumns)
     console.log("in post controller")
     $scope.loadPost = (post_id) => {
       $state.go('dash.showPost', { feed_name: getFeed, post_id })

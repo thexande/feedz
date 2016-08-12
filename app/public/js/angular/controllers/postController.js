1938781e96e18a138213ae086e5d4fbfd4e1ca1c
Alexander Murphy <amurphy345@gmail.com>
2016-08-12 00:17:42 -0600

    adding pace

--- app/public/js/angular/controllers/postController.js
+++ app/public/js/angular/controllers/postController.js
@@ -1,5 +1,6 @@
   angular.module('feedz.postController', [])
   .controller('postController', function(getFeedById, $scope, $mdDialog, $log, userFactory, $state) {
     console.log(getFeedById)
+    $scope.feed = getFeedById.data
     console.log("in post controller")
   })

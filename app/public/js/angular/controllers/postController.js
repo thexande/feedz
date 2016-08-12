6cd0bec9dfdfbe48e080af1c569b20acb709e82a
Alexander Murphy <amurphy345@gmail.com>
2016-08-12 13:25:10 -0600

    working on nested ng repeat for cardsd

--- app/public/js/angular/controllers/postController.js
+++ app/public/js/angular/controllers/postController.js
@@ -4,6 +4,14 @@
     console.log(getFeed)
     $scope.feedName = getFeed.feed_name
     $scope.feed = getFeedById.data
+
+    $scope.feedColumnLength = $scope.feed.length / 3
+    $scope.feedColumns = []
+    
+    while($scope.feed.length) {
+      $scope.feedColumns.push($scope.feed.splice(0, $scope.feedColumnLength))
+    }
+
     console.log("in post controller")
     $scope.loadPost = (post_id) => {
       $state.go('dash.showPost', { feed_name: getFeed, post_id })

44a5e32bba84ea4ebbe910928deef76b06f2950b
Alexander Murphy <amurphy345@gmail.com>
2016-08-12 13:52:21 -0600

    icons added

--- app/public/js/angular/controllers/postController.js
+++ app/public/js/angular/controllers/postController.js
@@ -1,7 +1,5 @@
   angular.module('feedz.postController', [])
   .controller('postController', function(getFeedById, getFeed, $scope, $mdDialog, $log, userFactory, $state) {
-    console.log(getFeedById)
-    console.log(getFeed)
     $scope.feedName = getFeed.feed_name
     $scope.feed = getFeedById.data
 
@@ -13,7 +11,6 @@
     }
     if($scope.feedColumns.length === 4){
       $scope.feedColumns[3].forEach((val, key) => {
-        console.log(val)
         $scope.feedColumns[ Math.floor(Math.random() * (2 - 0 + 1)) + 0].push(val)
       })
       $scope.feedColumns.pop()

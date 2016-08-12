809c03f5e73d0dbfdb9720fa0807adb3d29c5c94
Alexander Murphy <amurphy345@gmail.com>
2016-08-11 19:26:58 -0600

    updating login and register pages

--- app/public/js/angular/controllers/subFeedController.js
+++ app/public/js/angular/controllers/subFeedController.js
@@ -20,6 +20,39 @@
     })
     .controller('subFeedController', function($scope, getSubFeeds, $mdDialog, userFactory) {
       console.log("subfeeds here", getSubFeeds)
+      $scope.subFeeds = getSubFeeds.data
+       $scope.activity = [
+      {
+        what: 'Brunch this weekejlkjljlkjnd?',
+        who: 'Ali Conners',
+        when: '3:08PM',
+        notes: " I'll be in youkljlkjlkr neighborhood doing errands"
+      },
+      {
+        what: 'Summer BBQ',
+        who: 'to Alex, Scott, Jennifer',
+        when: '3:08PM',
+        notes: "Wish I could comlkjlkjlkje out but I'm out of town this weekend"
+      },
+      {
+        what: 'Oui Oui',
+        who: 'Sandra Adams',
+        when: '3:08PM',
+        notes: "Do youkljlkjlkjlkj have Paris recommendations? Have you ever been?"
+      },
+      {
+        what: 'Birthday Gift',
+        who: 'Trevor Hansen',
+        when: '3:08PM',
+        notes: "Have any ideas of what we should get Heidi for her birthday?"
+      },
+      {
+        what: 'Recipe to try',
+        who: 'Brian Holt',
+        when: '3:08PM',
+        notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
+      },
+    ];
       $scope.showAdd = function(ev) {
         $mdDialog.show({
             controller: 'feedz.subFeedModalController',

a329306efb0f5fba4f558c26657ae6d53b5153cd
Alexander Murphy <amurphy345@gmail.com>
2016-08-14 14:09:52 -0600

    post cards restfully updating

--- app/public/js/angular/controllers/postController.js
+++ app/public/js/angular/controllers/postController.js
@@ -12,31 +12,26 @@
     localStorageFactory) {
     // subfeeds for dropdown
     $scope.subFeeds = subfeeds.data
-    console.log(subfeeds)
-    console.log(getFeedById)
     $scope.feedPristine = getFeedById.data
     $scope.feed = getFeedById.data
     $scope.feeds = getFeedById.data.posts
-    // $scope.feedColumnLength = $scope.feed.posts.length / 3
-    console.log($scope.feedColumnLength)
-    $scope.feedColumns = []
-    console.log($scope.feed.posts)    
 
+    $scope.generateColumns = (feed) => {
+      $scope.feedColumns = []
+      $scope.feedColumnLength = feed.length / 3
+      while(feed.length) {
+        $scope.feedColumns.push(feed.splice(0, $scope.feedColumnLength))
+      }
+      if($scope.feedColumns.length === 4){
+        $scope.feedColumns[3].forEach((val, key) => {
+          $scope.feedColumns[ Math.floor(Math.random() * (2 - 0 + 1)) + 0 ].push(val)
+        })
+        $scope.feedColumns.pop()
+      }
+     return [].concat.apply([], $scope.feedColumns)
+    }
 
-    
-      // while($scope.feed.posts.length) {
-      //   $scope.feedColumns.push($scope.feed.posts.splice(0, $scope.feedColumnLength))
-      // }
-      // console.log($scope.feedColumns)
-      // if($scope.feedColumns.length === 4){
-      //   $scope.feedColumns[3].forEach((val, key) => {
-      //     $scope.feedColumns[ Math.floor(Math.random() * (2 - 0 + 1)) + 0].push(val)
-      //   })
-      //   $scope.feedColumns.pop()
-      // }
-      // $scope.feeds = [].concat.apply([], $scope.feedColumns)
-      // console.log($scope.feeds)
-
+    $scope.feeds = $scope.generateColumns($scope.feed.posts)
     
     $scope.loadPost = (post_id) => {
       $state.go('dash.showPost', { feed_name: getFeed, post_id })
@@ -49,7 +44,7 @@
           targetEvent: ev,
         })
     }
-    $scope.showAddPost = function(ev) {
+  $scope.showAddPost = function(ev) {
       $mdDialog.show({
           controller: 'feedz.postModalController',
           templateUrl: '/build/addPostModal.html',
@@ -58,10 +53,8 @@
           },
           targetEvent: ev,
         }).then((postResp) => {
-          subFeedFactory.getPostsForFeed(getFeedById.data.id).then((feed) => {
-            console.log(feed)
-            $scope.feeds = feed.data.posts
-          })
+          subFeedFactory.getPostsForFeed(getFeedById.data.id)
+          .then((updatedFeed) => $scope.feeds = $scope.generateColumns(updatedFeed.data.posts))
         })
     }
 
